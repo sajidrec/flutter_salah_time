e20a8604e61aae6c4136c11f07489b8018117aa7
MD. Sajid Hossain <sajidrec@gmail.com>
2024-05-09 16:10:00 +0600

    tapping on history element gives details

--- lib/presentation/screens/search_history_screen.dart
+++ lib/presentation/screens/search_history_screen.dart
@@ -59,50 +59,194 @@ class _SearchHistoryScreenState extends State<SearchHistoryScreen> {
                     shrinkWrap: true,
                     itemCount:
                         searchHistoryScreenController.listOfHistory.length,
-                    itemBuilder: (context, index) => Card(
-                      color: primaryAppColor.shade400,
-                      elevation: 3,
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Row(
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "${searchHistoryScreenController.listOfHistory[index].placeName.substring(1, searchHistoryScreenController.listOfHistory[index].placeName.length - 1)}",
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 22,
-                                    color: primaryAppTextColor,
+                    itemBuilder: (context, index) => GestureDetector(
+                      onTap: () {
+                        Get.dialog(AlertDialog(
+                          backgroundColor: primaryAppColor,
+                          contentTextStyle: const TextStyle(
+                              color: primaryAppTextColor, fontSize: 25),
+                          titleTextStyle: const TextStyle(
+                            color: primaryAppTextColor,
+                            fontSize: 27,
+                          ),
+                          title: Text(
+                            searchHistoryScreenController
+                                .listOfHistory[index].placeName
+                                .substring(
+                                    1,
+                                    searchHistoryScreenController
+                                            .listOfHistory[index]
+                                            .placeName
+                                            .length -
+                                        1),
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          // TODO: refactor the code
+                          content: Column(
+                            children: [
+                              Row(
+                                children: [
+                                  const Text(
+                                    "Fajr",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  "${searchHistoryScreenController.listOfHistory[index].date}",
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w500,
-                                    color: primaryAppTextColor,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const Spacer(),
-                            IconButton(
-                              onPressed: () {
-                                deleteHistoryElementConfirmDialog(
-                                  message:
-                                      "You sure want to delete this history?",
-                                  index: index,
-                                );
-                              },
-                              icon: const Icon(
-                                Icons.delete,
-                                size: 30,
-                                color: primaryAppTextColor,
+                                  const Spacer(),
+                                  Text(
+                                    "${searchHistoryScreenController.listOfHistory[index].fajr}",
+                                    style: const TextStyle(),
+                                  )
+                                ],
                               ),
-                            ),
-                          ],
+                              const SizedBox(
+                                height: 3,
+                              ),
+                              Row(
+                                children: [
+                                  const Text(
+                                    "Shurooq",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w600),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    "${searchHistoryScreenController.listOfHistory[index].shurooq}",
+                                    style: const TextStyle(),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(),
+                              Row(
+                                children: [
+                                  const Text(
+                                    "Dhuhr",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w600),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    "${searchHistoryScreenController.listOfHistory[index].dhuhr}",
+                                    style: const TextStyle(),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(),
+                              Row(
+                                children: [
+                                  const Text(
+                                    "Asr",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w600),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    "${searchHistoryScreenController.listOfHistory[index].asr}",
+                                    style: const TextStyle(),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(),
+                              Row(
+                                children: [
+                                  const Text(
+                                    "Maghrib",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w600),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    "${searchHistoryScreenController.listOfHistory[index].maghrib}",
+                                    style: const TextStyle(),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(),
+                              Row(
+                                children: [
+                                  const Text(
+                                    "Isha",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w600),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    "${searchHistoryScreenController.listOfHistory[index].isha}",
+                                    style: const TextStyle(),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 25,
+                              ),
+                              SizedBox(
+                                width: double.maxFinite,
+                                child: ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStateProperty.all(Colors.red),
+                                  ),
+                                  onPressed: () {
+                                    Get.back();
+                                  },
+                                  child: const Text(
+                                    "Close",
+                                    style: TextStyle(
+                                      color: primaryAppTextColor,
+                                      fontSize: 22,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ));
+                      },
+                      child: Card(
+                        color: primaryAppColor.shade400,
+                        elevation: 3,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "${searchHistoryScreenController.listOfHistory[index].placeName.substring(1, searchHistoryScreenController.listOfHistory[index].placeName.length - 1)}",
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 22,
+                                      color: primaryAppTextColor,
+                                    ),
+                                  ),
+                                  Text(
+                                    "${searchHistoryScreenController.listOfHistory[index].date}",
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500,
+                                      color: primaryAppTextColor,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Spacer(),
+                              IconButton(
+                                onPressed: () {
+                                  deleteHistoryElementConfirmDialog(
+                                    message:
+                                        "You sure want to delete this history?",
+                                    index: index,
+                                  );
+                                },
+                                icon: const Icon(
+                                  Icons.delete,
+                                  size: 30,
+                                  color: primaryAppTextColor,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
